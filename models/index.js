33ad515d0568f7c68629458a812427bbc0353dec
woffordlm <lukewofford@gmail.com>
2022-02-08 12:01:20 -0500

    end of section 3

--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
 const User = require('./User');
 const Post = require('./post');
-
+// create associations
+User.hasMany(Post, {
+    foreignKey:'user_id'
+})
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
 module.exports = { User, Post };
