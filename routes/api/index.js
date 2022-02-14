df24a3eee377c46e21e3dba538f0ae1cbd31dca0
woffordlm <lukewofford@gmail.com>
2022-02-14 16:55:46 -0500

    start of mvm module

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const postRoutes = require ('./post-routes')
+
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes)
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
